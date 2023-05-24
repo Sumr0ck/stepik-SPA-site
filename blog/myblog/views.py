b9ea45c8845842a164d58254f4a980e838d00d14
Sumr0ck <sumrock1@mail.ru>
2023-05-24 18:47:20 +0300

    register page

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+
+from .forms import SignUpForm
 from .models import Post
 
 
@@ -23,3 +27,23 @@ class PostDetailView(View):
     def get(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, url=slug)
         return render(request, 'myblog/post_detail.html', context={'post': post})
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+    
